34201bd2cf0e86f9b9e381ecdeabe1af96668651
roman <romanzukov1993@gmail.com>
2018-03-08 21:54:15 +0100

    Display the data in the table and fixed day format

--- src/app/components/date-filter/date-filter.component.ts
+++ src/app/components/date-filter/date-filter.component.ts
@@ -44,7 +44,7 @@ export class DateFilterComponent implements OnInit {
    */
   public getDayMonth = (data, year) => {
     const yearMonths = {0: 'Jan', 1: 'Feb', 2: 'Mar', 3: 'Abr', 4: 'May', 5: 'Jun', 6: 'Jul', 7: 'Aug', 8: 'Sep', 9: 'Oct', 10: 'Nov', 11: 'Dec'};
-    const weekDays = {0: 'Mon', 1: 'Tue', 2: 'Wed', 3: 'Thu', 4: 'Fri', 5: 'Sat', 6: 'Sun'};
+    const weekDays = {1: 'Mon', 2: 'Tue', 3: 'Wed', 4: 'Thu', 5: 'Fri', 6: 'Sat', 7: 'Sun'};
     let filteredDates: Array<string>;
     const dayMonth: Array<{}> = [];
 
--- src/app/components/result-table/result-table.component.ts
+++ src/app/components/result-table/result-table.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { LottolandServiceService } from "../../services/lottoland-service.service";
+import * as _ from 'lodash';
 
 @Component({
   selector: 'app-result-table',
@@ -6,10 +8,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./result-table.component.css']
 })
 export class ResultTableComponent implements OnInit {
+  private romanNumbers: {};
+  private matches: {};
+  private titles: any;
+  private dataToDraw: any;
 
-  constructor() { }
+  constructor(private lottolandService: LottolandServiceService) {
+    this.romanNumbers = { 1: 'I', 2: 'II', 3: 'III', 4: 'IV', 5: 'V', 6: 'VI', 7: 'VII', 8: 'VIII', 9: 'IX', 10: 'X', 11: 'XI', 12: 'XII' };
+    this.matches = {1: {n: 5, e: 2}, 2: {n: 5, e: 1}, 3: {n: 5, e: 0}, 4: {n: 4, e: 2}, 5: {n: 4, e: 1}, 6: {n: 4, e: 0}, 7: {n: 3, e: 2},
+      8: {n: 2, e: 2}, 9: {n: 3, e: 1}, 10: {n: 3, e: 0}, 11: {n: 1, e: 2}, 12: {n: 2, e: 1}};
+    this.titles = {tier: 'Tier', match: 'Match', winners: 'Winners', amount: 'Amount'};
+  }
+
+  /**
+   * Order and parse the received data
+   * @param dataToParse - Received data
+   */
+  public parseAndDrawInfo = (dataToParse) => {
+    this.dataToDraw = _.values(dataToParse[0].odds);
+    this.dataToDraw = _.orderBy(this.dataToDraw, ['prize'], ['desc']);
+    this.dataToDraw = _.filter(this.dataToDraw, (d) => {return d.prize !== 0});
+  }
 
   ngOnInit() {
+    this.lottolandService.getRowData().subscribe(data => {
+      if (data && data.length > 0)
+        this.parseAndDrawInfo(data);
+    });
   }
 
 }
